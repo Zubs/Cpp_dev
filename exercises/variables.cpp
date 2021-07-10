7538a4630d1aee4d5c7c91bc35b96d1431875c28
Zubs <zubairidrisaweda@gmail.com>
2021-07-11 00:54:20 +0100

    First exercise attempt

---
+++ exercises/variables.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //define your variables here
+    int intNumber = 30;
+    float floatNumber = 30.78;
+    double doubleNumber = 45.1234;
+    bool boolean = true;
+    char charName = 'u';
+
+    cout << "Value of Integer is: " << intNumber;
+    // cout your integer variable here
+    cout << endl;
+    
+    cout << "Value of Float is: " << floatNumber;
+    // cout your float variable here
+    cout << endl;
+    
+    cout << "Value of Double is: " << doubleNumber;
+    // cout your double variable here
+    cout << endl;
+    
+    cout << "Value of Char is: " << charName;
+    // cout your char variable here
+    cout << endl;
+    
+    cout << "Value of Bool is: " << boolean;
+    // cout bool double variable here
+    cout << endl;
+    
+    return 0;
+}
