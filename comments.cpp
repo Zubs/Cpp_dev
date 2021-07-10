7232c271b3c963c224e3f4afa7131860a7005718
Zubs <zubairidrisaweda@gmail.com>
2021-07-10 21:51:01 +0100

    Comments

---
+++ comments.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+using namespace std;
+
+int main () {
+    /*
+        Multi-line comments
+        You get the gist now
+    */
+
+    // Single line comment
+
+    cout << "Learned Comments" << endl;
+    return 0;
+}
