bb816ec41a702c20da057ca335556d2ed9430002
Zubs <zubairidrisaweda@gmail.com>
2021-07-12 00:38:54 +0100

    Getting inputs

---
+++ inputs.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+
+using namespace std;
+
+int main () {
+
+    // Declare variable
+    int age;
+
+    // Print simple question
+    cout << "How old are you? " << endl;
+
+    // Get user input
+    cin >> age;
+
+    // Return simple message
+    cout << "You are " << age << " years old" << endl;
+    
+    return 0;
+}
