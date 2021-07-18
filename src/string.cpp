2151632c9e0b1a38f4810d1318914f27f3b4cd75
Zubs <zubairidrisaweda@gmail.com>
2021-07-18 14:52:41 +0100

    Strings

---
+++ src/string.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+# include <string>
+# include <sstream>
+
+using namespace std;
+
+int main () {
+
+    string stringInput;
+    float numericValue;
+
+    cout << "Enter a number as string: " << endl;
+    getline (cin, stringInput);
+
+    stringstream(stringInput) >> numericValue;
+    
+    cout << "You entered " << stringInput << endl;
+
+    cout << "Your value as float is " << numericValue << endl;
+    
+    return 0;
+};
