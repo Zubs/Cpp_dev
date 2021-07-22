cbcbb9f26e0d0f17737484a36c76dc1c02c1f46b
Zubs <zubairidrisaweda@gmail.com>
2021-07-22 10:44:58 +0100

    Variables

---
+++ quizes/variable.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+
+using namespace std;
+
+int main () {
+    float width = 4.5;
+    float height = 5.5;
+
+    int area = width * height;
+    cout << width * height << endl;
+    cout << "area = " << area << endl;
+
+    int numerator = 4;
+    int denominator = 5;
+
+    int answer = numerator / denominator;
+    cout << numerator / denominator << endl;
+    cout << "answer = " << answer << endl;
+    
+    return 0;
+}
