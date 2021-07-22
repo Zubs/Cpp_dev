146bdeb1e97d7b8bfbb162b18240fce0c2142abe
Zubs <zubairidrisaweda@gmail.com>
2021-07-22 03:25:16 +0100

    Basic Maths

---
+++ src/arithmetic.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+# include <cmath>
+
+using namespace std;
+
+int main () {
+
+    // Power, 5^3
+    cout << pow(5, 3) << endl;
+
+    // Pi: M_PI
+    cout << "Pi: " << M_PI << endl;
+
+    // Modulus, to get remainder
+    cout << 5 % 3 << endl;
+
+    return 0;
+};
